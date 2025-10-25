282d0e436085603555f79aef9f4f6ed8ba759a59
8bitmudit <ditsharma7077@gmail.com>
2025-10-25 23:57:09 +0530

    Day1-Practice

---
+++ largestEleInArray.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int largest(vector<int> arr) {
+    int m = 0;
+    m = arr[0];
+    for(int i = 0;i<arr.size();i++)
+    {
+        if(m < arr[i]){
+            m =arr[i];
+        }
+
+    }
+    
+return m;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    int k;
+    int x;
+    vector<int> nums;
+    cin>> n;
+
+    for(int i=0; i<n ; i++){
+        cin>> x;
+        nums.push_back(x);
+    }
+    
+
+    int ans = largest(nums);
+    cout<<"Bhai answer: "<< ans;
+
+    return 0;
+}
