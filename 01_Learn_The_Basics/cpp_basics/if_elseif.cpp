a34e20604e08c2d923139ff41b426d267c5c92e8
8bitmudit <ditsharma7077@gmail.com>
2026-08-23 23:30:48 +0530

    test push

---
+++ 01_Learn_The_Basics/cpp_basics/if_elseif.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void studentGrade(int marks) {
+    int x = marks;
+    if(x >=90){
+        cout<<"Grade A";
+    }
+    else if(x >=70){
+        cout<<"Grade B";
+    }
+    else if(x >=50){
+        cout<<"Grade C";
+    }
+    else if(x >=35){
+        cout<<"Grade D";
+    }
+    else if(x<35){
+        cout<<"Fail";
+    }
+    
+}
+int main() {
+    int marks;
+    cin>>marks;
+    studentGrade(marks);
+    
+    return 0;
+}
